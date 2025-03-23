924fd8f3f36c31ffcb3927c7facf1b7670b3fdca
zhenllyalvarez <dev.zhenllyalvarez@gmail.com>
2025-03-23 14:26:20 +0800

    added logic

--- js/logic.js
+++ js/logic.js
@@ -1,3 +1,12 @@
 export function calculateTotal(value) {
-    return value * 10;
+    let total = 0;
+
+    if(value <= 10) {
+        total = value * 10;
+    } else if (value <= 20) {
+        total = value * 9.50;
+    } else {
+        total = value * 9;
+    }
+    return total;
 }
